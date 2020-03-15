75f3b489169dfaf65a2fbae2fa9c6680786136cf
syuan0212@gmail.com <hsuan777>
2020-03-15 16:51:22 +0800

    修改__增加td顯示資料

--- js/hexBMI.js
+++ js/hexBMI.js
@@ -11,37 +11,54 @@ submitBtn.addEventListener('click', function () {
   var height = heightElement.value / 100;
   // 計算 BMI = weight / height*height 單位: 公斤 / 米平方
   var bmi = (weight / (height * height)).toFixed(1);
-  
+
   if (weight == "" || height == "") {
-      alert("請先輸入數字");
+    alert("請先輸入數字");
   } else {
-    // alert("您的BMI="+bmi+"體重:"+weight+"身高:"+heightElement.value);  
+    // alert("您的BMI="+bmi+"體重:"+weight+"身高:"+heightElement.value); 
+
+    // 抓取表格
     var table__BMI = document.querySelector('.table__BMI');
+
+    // 建立節點
     var tbody = document.createElement("tbody");
     var tr = document.createElement("tr");
     var th = document.createElement("th");
-    var tdWeight = document.createElement("td").setAttribute('id','table-weight');
-    var tdHeight = document.createElement("td").setAttribute('id','table-height');
-    // th.textContent = "理想";
-    // td.textContent = "BMI:" + bmi;  
-    // html
     table__BMI.appendChild(tbody).appendChild(tr).appendChild(th);
-    
-
-    document.getElementById('table-weight').textContent = weight;
-    document.getElementById('table-height').textContent = height;
-    // 如何回寫不同 td值
-    // CSS  
+    // 判斷要顯示的狀態
     if (bmi < 18.5) {
-        tr.classList.add('table-underweight');
-        th.textContent = "過輕";
+      tr.classList.add('table-underweight');
+      th.textContent = "過輕";
     } else if (bmi >= 18.5 && bmi <= 24.9) {
-        th.textContent = "理想";
-        tr.classList.add('table-success');
+      th.textContent = "理想";
+      tr.classList.add('table-success');
     } else if (bmi > 24.9) {
-        th.textContent = "危險";
-        tr.classList.add('table-danger');
-    }  
+      th.textContent = "危險";
+      tr.classList.add('table-danger');
+    }
+
+    // 建立 3個td -> BMI、體重、身高
+    var tdBMI = document.createElement("td");
+    tdBMI.className = 'table-BMI';
+    var textBMI = document.createTextNode('BMI : ' + bmi);
+    tdBMI.appendChild(textBMI);
+    tr.appendChild(tdBMI);
+
+    // var tdWeight = document.createElement("td").setAttribute('id','table-weight');
+    var tdWeight = document.createElement("td");
+    tdWeight.className = 'table-weight';
+    var textWeight = document.createTextNode('體重 : ' + weight);
+    tdWeight.appendChild(textWeight);
+    tr.appendChild(tdWeight);
+
+    // var tdHeight = document.createElement("td").setAttribute('id','table-height');
+    var tdHeight = document.createElement("td");
+    tdHeight.className = 'table-height';
+    var textHeight = document.createTextNode('身高 : ' + heightElement.value);
+    tdHeight.appendChild(textHeight);
+    tr.appendChild(tdHeight);
+  
+
   }
 })
 
